8e332ed5314d74eb93998c7025a8532468d8a9d2
Tejas <tejasa755@gmail.com>
2024-12-13 20:56:25 +0530

    first commit

---
+++ server/utils/generateToken.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+config();
+
+export const generateToken = (res, user, message) => {
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  return res
+    .status(200)
+    .cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    }).json({
+        success:true,
+        message,
+        user
+    });
+};
